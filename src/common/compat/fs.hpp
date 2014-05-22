e941ce04dc6a275687e2f4dede6a97eac0cac3db
simplex <theemptystring@gmail.com>
2014-05-23 00:55:19 +0100

    win fix at timestamp comparison

--- src/common/compat/fs.hpp
+++ src/common/compat/fs.hpp
@@ -363,9 +363,7 @@ namespace Compat {
 				return mybuf.st_mtimespec.tv_nsec > otherbuf.st_mtimespec.tv_nsec;
 #endif
 			}
-			else {
-				return false;
-			}
+			return false;
 		}
 
 		bool isOlderThan(const PathAbstraction& p) const {
